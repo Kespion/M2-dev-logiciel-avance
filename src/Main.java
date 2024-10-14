c85457becb5076d7fc3949d4dfdcb5915dfaf5a0
Kespion <kesputecryo@gmail.com>
2024-10-14 22:42:47 +0200

    test 3

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
     public static void ScenarioReentrantLock() throws InterruptedException {
         List<Fourchette> fourchettes = new ArrayList<>();
         for (int i = 0; i < NOMBRE_DE_PHILOSOPHES; i++) {
-            fourchettes.add(new Fourchette("Fourchette " + i));
+            fourchettes.add(new Fourchette("Fourchett " + i));
         }
 
         List<Philosophe> philosophes = new ArrayList<>();
