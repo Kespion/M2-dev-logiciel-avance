f226b1a679b5984c8de18f396077b00f104773bc
Anthony VIANO <anthony.viano@amadeus.com>
2024-10-15 17:32:36 +0200

    reset

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Semaphore;
-
 public class Main {
 
-    public static final int NOMBRE_CLIENTS = 10;
-    public static final int NOMBRE_CABINES = 5;
-    public static final int NOMBRE_PANIERS = 5;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        Semaphore cabines = new Semaphore(NOMBRE_CABINES);
-        Semaphore paniers = new Semaphore(NOMBRE_PANIERS);
-
-        List<Client> clients = new ArrayList<>();
-        for (int i = 0; i < NOMBRE_CLIENTS; i++) {
-            clients.add(new Client("Client " + i, cabines, paniers));
-        }
-
-        for (Client client : clients) {
-            client.start();
-        }
-
-        for (Client client : clients) {
-            client.join();
-        }
+        System.out.println("Hello World!");
 
     }
 
