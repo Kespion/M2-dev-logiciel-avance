9336ce51ee87b593a103070b32acf1663b8081c9
Kespion <kesputecryo@gmail.com>
2024-10-25 15:32:05 +0200

    la piscine

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static final int NOMBRE_CLIENTS = 10;
+    public static final int NOMBRE_CABINES = 5;
+    public static final int NOMBRE_PANIERS = 5;
 
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws InterruptedException {
+
+        Piscine piscine = new Piscine(NOMBRE_CABINES, NOMBRE_PANIERS);
+
+        List<Client> clients = new ArrayList<>();
+        for (int i = 0; i < NOMBRE_CLIENTS; i++) {
+            clients.add(new Client("Client " + i, piscine));
+        }
+
+        for (Client client : clients) {
+            client.start();
+        }
+
+        for (Client client : clients) {
+            client.join();
+        }
 
     }
 
