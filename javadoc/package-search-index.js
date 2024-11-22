27ef19d73d8bb0d4d03b2a12f13485cc4f5ba4bf
Kespion <kesputecryo@gmail.com>
2024-11-22 15:32:03 +0100

    javadoc generated

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.game"},{"l":"com.game.cards"},{"l":"com.game.dao"},{"l":"com.game.matches"},{"l":"com.game.players"},{"l":"com.game.repository"},{"l":"com.game.utils"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.game.cards","l":"Card"},{"p":"com.game.repository","l":"CardRepository"},{"p":"com.game.utils","l":"GameUtils"},{"p":"com.game","l":"Main"},{"p":"com.game.matches","l":"Match"},{"p":"com.game.players","l":"Player"},{"p":"com.game.dao","l":"PlayerDAO"}];updateSearchResults();
